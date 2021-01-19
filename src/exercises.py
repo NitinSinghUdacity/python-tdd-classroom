6d7750dd19f5ddf83d9f5194310f2e45ec8c8eca
Nitin Singh <49961095+NitinSinghUdacity@users.noreply.github.com>
2021-01-19 13:35:23 +0530

    Update exercises.py

--- src/exercises.py
+++ src/exercises.py
@@ -2,15 +2,10 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    input_list=input_list[::-1]
-    return input_list
+    return None
 
 def count_digits(number):
     """
     Return count of digits
     """
-    count=0
-    while number:
-        count+=1
-        number=number//10
-    return count
+    return None
